7e09caa39be56f39279a2a36c2d22ae8e1def8de
ConnorPekovic <60188120+connorpekovic@users.noreply.github.com>
2022-07-17 19:14:57 -0400

    Image Scale and Desaturate
    Image Scale and De saturate. Default Cartoon plugin (plugin-in-cartoon) and 3rd party scripts prove ineffective.

--- plugin.py
+++ plugin.py
@@ -3,6 +3,20 @@
 from gimpfu import *
 
 def plugin_func(image, drawable):
+
+    
+    #resize image to 750x750
+    pdb.gimp_image_scale(image, 750, 750)
+    
+    #desaturate
+    pdb.gimp_drawable_desaturate(drawable, 2)
+
+    #cartoonize
+    #pdb.plug_in_cartoon(0, image, drawable, 10.000, 0.200)
+
+    #export to .jpg
+    #pdb.file_jpeg_save(1, image, drawable, , , , , , , , , ,)
+
     return
               
 # do what plugins do best
